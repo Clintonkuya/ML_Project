89ad35b5b516e0193e40ca06cf2aa638ac2a8828
Clintonkuya <clintonmwangi17@gmail.com>
2024-01-26 01:42:06 +0300

    loggin and exception

---
+++ source/exception.py
@@ -0,0 +1,21 @@
+import sys #provide the various functions and variable that are used to manipulate different part of python runtime envi.
+import logging
+
+def error_message_detail(error,error_detail:sys):
+    '''This is a custom-made made error exception'''
+    _,_,exc_tb=error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message="Error have occured in python script name [{0}] line number [{1} error message[{2}]".format(
+        file_name,exc_tb.tb_lineno,str(error))
+    return error_message
+    
+class CustomException(Exception):#Inherit the in built class Exception
+    def __init__(self, error_message,error_detail:sys) :
+        super().__init__(error_message) 
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    def __str__(self):
+        return self.error_message
+    
+
+    
+  
